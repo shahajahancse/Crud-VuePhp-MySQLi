9dc20482e2525623c02a1b60b8945f0fcde0e0e3
shahajahancse <shahajahancse@gmail.com>
2019-10-29 12:26:15 +0600

    data inserted by vuejs and axios method

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var app = new Vue({
   	this.getAllUsers();
   },
   methods: {
-  	getAllUsers(){
+  	getAllUsers(){  
   		axios.get("http://127.0.0.1/Crud-vue-php/db.php?action=read").then(function(response) {
   			if (response.data.error) {
   				app.errorMsg = response.data.message;
@@ -24,6 +24,28 @@ var app = new Vue({
   				app.users = response.data.users;
   			}
   		});
-  	}
+  	},
+  	// here add user
+  	addUser(){
+  		var formData = app.toFormData(app.newUser);
+  		axios.post("http://127.0.0.1/Crud-vue-php/db.php?action=create", formData).then(function(response) {
+  			app.newUser = {name: "" ,email: "" ,phone: ""};
+  			if (response.data.error) {
+  				app.errorMsg = response.data.message;
+  			}
+  			else
+  			{
+  				app.successMsg = response.data.message;
+  				app.getAllUsers();
+  			}
+  		});
+  	},
+  	toFormData(obj){
+  		var fd = new FormData();
+  		for (var i in obj) {
+  			fd.append(i, obj[i]);
+  		}
+  		return fd;
+  	},
   }
-})
+});
