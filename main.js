4dfe941ee3c28b5be9afaec41636b1d13a6a6e6c
shahajahancse <shahajahancse@gmail.com>
2019-10-29 11:00:29 +0600

    data read complete from db by vuejs and axios method

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 var app = new Vue({
   el: '#app',
   data: {
-    errorMsg: false,
-    successMsg: false,
+    errorMsg: "",
+    successMsg: "",
     showAddModal: false,
     editAddModal: false,
-    editDeleteModal: false
+    editDeleteModal: false,
+    users: [],
+    newUser: {name: "", email: "", phone: ""},
+    currentUser: {}
+  },
+  mounted: function(){
+  	this.getAllUsers();
+  },
+  methods: {
+  	getAllUsers(){
+  		axios.get("http://127.0.0.1/Crud-vue-php/db.php?action=read").then(function(response) {
+  			if (response.data.error) {
+  				app.errorMsg = response.data.message;
+  			}
+  			else
+  			{
+  				app.users = response.data.users;
+  			}
+  		});
+  	}
   }
 })
